0aabea8ad926cf7a3bf7ddec212fc9ffddd350c5
farshadmoosavi <97333681+farshadmoosavi@users.noreply.github.com>
2023-06-30 18:49:34 +0330

    test

---
+++ Interfaces/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using accounting.Models;
+
+namespace accounting.Repository
+{
+	public interface ICustomerRepository
+	{
+            Customer GetById(int id);
+            void Add(Customer customer);
+            void Update(Customer customer);
+            void Delete(Customer customer);
+        
+    }
+}
+
